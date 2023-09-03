3dae5b2e6b0285576b3e3def829b1c25f20e6e5a
jacky2256 <a.bojic22@gmail.com>
2023-09-04 02:10:36 +0300

    add static

---
+++ static/js/script.js
@@ -0,0 +1,16 @@
+function changeImage(element) {
+    var largeImage = document.getElementById("largeImage");
+    largeImage.src = element.src;
+}
+
+
+
+// document.querySelectorAll('.dropdown-item').forEach(function (item) {
+//     item.addEventListener('click', function (e) {
+//         e.preventDefault();
+//         var languageCode = this.getAttribute('data-language-code');
+//         document.getElementById('language-code-input').value = languageCode;
+//         document.getElementById('language-switch-form').submit();
+//     });
+// });
+
