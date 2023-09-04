438abc5575148179972c61007e4de7c8987c4ff9
jacky2256 <a.bojic22@gmail.com>
2023-09-05 00:25:41 +0300

    coment staticfiles_dirs

--- config/settings.py
+++ config/settings.py
@@ -142,7 +142,7 @@ LOCALE_PATHS = [
 
 STATIC_URL = 'static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
+# STATICFILES_DIRS = [STATIC_DIR]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
