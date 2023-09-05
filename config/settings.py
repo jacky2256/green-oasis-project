9d981ce95518cf4d6aed264215997455ad169024
jacky2256 <a.bojic22@gmail.com>
2023-09-05 21:32:07 +0300

    add meadia files 1

--- config/settings.py
+++ config/settings.py
@@ -142,8 +142,8 @@ LOCALE_PATHS = [
 
 STATIC_URL = 'static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
-STATIC_ROOT = os.path.join(BASE_DIR, 'prod_static')
+# STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
