e2a2d7a3e3f12b878057454c00a787f384f1593a
jacky2256 <a.bojic22@gmail.com>
2023-09-06 22:14:30 +0300

    add load_roses_images.py

--- roses/management/commands/load_roses_images.py
+++ roses/management/commands/load_roses_images.py
@@ -10,6 +10,29 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 django.setup()
 
 
+# class Command(BaseCommand):
+#     help = 'Load data from CSV file'
+
+#     def handle(self, *args, **kwargs):
+#         with open('roses/management/commands/roses_images.csv', 'r', encoding='utf-8') as csv_file:
+#             csv_reader = csv.DictReader(csv_file)
+#             for row in csv_reader:
+#                 category_name = row['slug']
+#                 rose = RoseModel.objects.get(
+#                     slug=category_name)
+
+#                 rose_images = RoseImages(
+#                     image = row['image'],
+#                     rose = rose
+#                 )
+#                 image_path = f"/home/jacky/Documents/GIT_Projects/green-oasis-2/green-oasis-project_scraping/output/{row['image']}"
+#                 with open(image_path, 'rb') as img_file:
+#                     rose_images.image.save(
+#                         row['image'], File(img_file), save=True)
+#                 rose_images.save()
+#         self.stdout.write(self.style.SUCCESS('Data loaded successfully'))
+
+
 class Command(BaseCommand):
     help = 'Load data from CSV file'
 
@@ -25,9 +48,9 @@ class Command(BaseCommand):
                     image = row['image'],
                     rose = rose
                 )
-                image_path = f"/home/jacky/Documents/GIT_Projects/green-oasis-2/green-oasis-project_scraping/output/{row['image']}"
-                with open(image_path, 'rb') as img_file:
-                    rose_images.image.save(
-                        row['image'], File(img_file), save=True)
+                # image_path = f"/home/jacky/Documents/GIT_Projects/green-oasis-2/green-oasis-project_scraping/output/{row['image']}"
+                # with open(image_path, 'rb') as img_file:
+                #     rose_images.image.save(
+                #         row['image'], File(img_file), save=True)
                 rose_images.save()
         self.stdout.write(self.style.SUCCESS('Data loaded successfully'))
