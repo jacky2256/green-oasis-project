6138193167c2e33d7a19d487f73261a0b448452a
jacky2256 <a.bojic22@gmail.com>
2023-09-21 13:32:21 +0300

    modify description detail

--- config/prod_setings.py
+++ config/prod_setings.py
@@ -1,2 +1,13 @@
+from pathlib import Path
+import environ
+import os
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 DEBUG = False
 ALLOWED_HOSTS = ['www.green-oasis.md']
+
+STATIC_URL = 'static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- config/settings.py
+++ config/settings.py
@@ -140,11 +140,6 @@ LOCALE_PATHS = [
 ]
 
 
-STATIC_URL = 'static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [STATIC_DIR]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
